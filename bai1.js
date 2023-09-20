b94d52a592a62f2e2099bea1377bf9d735a949c2
filejsbasic <maingoclu@gmail.com>
2023-09-20 19:42:56 +0700

    bb

---
+++ bai1.js
@@ -0,0 +1,39 @@
+// //bai1.a
+// console.log('Chao ACE')
+// //bai1.b
+// console.log('Mai Ngoc Lu')
+// //bai1.c
+// console.clear()
+// //bai1.d
+// console.log('*****')
+// console.log('*****')
+// console.log('*****')
+// console.log('*****')
+// console.log('*****')
+// console.log('*****')
+// //bai1.e
+// console.clear()
+// console.log('    *')
+// console.log('   ***')
+// console.log('  *****')
+// console.log(' *******')
+// console.log('*********')
+// console.clear()
+// console.log(' ** ** ')
+// console.log('*  *  *')
+// console.log('*     *')
+// console.log(' *   * ')
+// console.log('  * *  ')
+// console.log('   *   ')
+let a = 10
+let b = '10'
+console.log(b)
+a = 20
+console.log(a)
+const pi = 3.14
+
+const pi = 5 
+console.log(pi)
+
+
+
