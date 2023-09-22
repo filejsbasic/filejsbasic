1347b994c1f624c3460dc461156b1a5cbfa8f776
filejsbasic <maingoclu@gmail.com>
2023-09-22 11:09:10 +0700

    cau1-2

--- bai1.js
+++ bai1.js
@@ -25,15 +25,32 @@
 // console.log(' *   * ')
 // console.log('  * *  ')
 // console.log('   *   ')
+
+//bai2a
 let a = 10
+//bai2b
 let b = '10'
 console.log(b)
+//bai2c
 a = 20
 console.log(a)
-const pi = 3.14
-
-const pi = 5 
+//bai2d
+// const pi = 3.14
+// console.log(pi)
+//bai2e
+const pi = 5
 console.log(pi)
+//bai2f
+let ten = 'iPhone14' , hsx = 'Apple', mau_sac = 'Trang', gia = '1000USD'
+//console.log(ten,hsx,mau_sac,gia)
+//bai2g
+gia = '750USD'
+//console.log(ten,hsx,mau_sac,gia)
+//bai2h
+ten = 'iPhone 14 promax'
+console.log(ten,hsx,mau_sac,gia)
+
+
 
 
 
