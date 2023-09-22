69623d0dd14504dcaba054a6431f1dab2da38174
filejsbasic <maingoclu@gmail.com>
2023-09-22 17:48:17 +0700

    1-3

--- bai1.js
+++ bai1.js
@@ -27,28 +27,66 @@
 // console.log('   *   ')
 
 //bai2a
-let a = 10
+// let a = 10
 //bai2b
-let b = '10'
-console.log(b)
-//bai2c
-a = 20
-console.log(a)
-//bai2d
-// const pi = 3.14
+// let b = '10'
+// console.log(b)
+// //bai2c
+// a = 20
+// console.log(a)
+// //bai2d
+// // const pi = 3.14
+// // console.log(pi)
+// //bai2e
+// const pi = 5
 // console.log(pi)
-//bai2e
-const pi = 5
-console.log(pi)
-//bai2f
-let ten = 'iPhone14' , hsx = 'Apple', mau_sac = 'Trang', gia = '1000USD'
-//console.log(ten,hsx,mau_sac,gia)
-//bai2g
-gia = '750USD'
-//console.log(ten,hsx,mau_sac,gia)
-//bai2h
-ten = 'iPhone 14 promax'
-console.log(ten,hsx,mau_sac,gia)
+// //bai2f
+// let ten = 'iPhone14' , hsx = 'Apple', mau_sac = 'Trang', gia = '1000USD'
+// //console.log(ten,hsx,mau_sac,gia)
+// //bai2g
+// gia = '750USD'
+// //console.log(ten,hsx,mau_sac,gia)
+// //bai2h
+// ten = 'iPhone 14 promax'
+// console.log(ten,hsx,mau_sac,gia)
+
+//bai3a
+let a = 2 , b = 3
+console.log(a+b, a-b, a*b, a/b)
+console.log('tong =',a+b)
+console.log('hieu =',a-b)
+console.log('tich =',a*b)
+console.log('thuong =',a/b)
+// // bai3b
+// console.log((a+b)/2)
+// //bai3c
+// let x = 3, y = 4, z = 6
+// console.log(x*x+2*x+1)
+// console.log(x**3-3*x*y-5*y+3*y**2)
+// console.log((x*y)**2-2*x**2*y+13*y)
+// console.log(4*x**3+3*x*y+y**2-(2*x**2-3*y))
+// console.log((5*x**2/4*x*y)+y**2)
+// console.log((x**2-2*z*x*y)/y**2+5*x-2*y**2+4*x*z**3+z**3)
+//bai3d
+// let r = 5
+// const pi = 3.14
+// console.log('dien tich, chu vi hinh tron la',pi*r**2,2*r*pi)
+//bai3e
+// let dai = 15,rong = 10
+// console.log('chu vi va dien tich hinh chu nhat la',(dai+rong)*2,dai*rong)
+//bai3f
+// let s = 120, tg = 130 
+// console.log('van toc xe la',120*60/130)
+//bai3g
+// console.log('qiang duong di duoc',150*60/60)
+// //bai3h
+// console.log('quang dg cano di dc',15,2*3)
+// //bai3i
+// console.log('do dai AB',((11-8)+1/3)*42)
+// //bai3j
+// console.log('the tich hinh khoi la',(3*8*5*6)+(8*5*6))
+
+
 
 
 
